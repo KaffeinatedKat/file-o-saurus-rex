7af6d084d3e11aaa6a94054b04ce6208981ad2dd
John Crawford <coffeeboi47@protonmail.com>
2023-04-20 08:31:59 -0600

    more stuff

---
+++ include/FileRex.h
@@ -0,0 +1,8 @@
+﻿// FileRex.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
---
+++ src/FileRex.cpp
@@ -0,0 +1,12 @@
+﻿// FileRex.cpp : Defines the entry point for the application.
+//
+
+#include "FileRex.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
